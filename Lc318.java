578656fce621de850b5b3928e206d935f96df032
AnujLo1c <anujlowanshi3@gmail.com>
2023-12-29 22:18:35 +0530

    lc1

---
+++ Lc318.java
@@ -0,0 +1,11 @@
+// 318. Maximum Product of Word Lengths
+public class Lc318{
+    public static int maxProduct(String[] words) {
+        
+        return 0;
+    }
+    public static void main(String[] args){
+        String[] s={"abcw","baz","foo","bar","xtfn","abcdef"};
+        System.out.println(maxProduct(s));
+    }
+}
