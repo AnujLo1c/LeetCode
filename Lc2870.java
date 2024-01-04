27c5b8f1fac7b6492fa950891bb900c0f2685ac4
AnujLo1c <anujlowanshi3@gmail.com>
2024-01-04 13:01:07 +0530

    q2

---
+++ Lc2870.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Lc2870 {
+    public static int minOperations(int[] nums) {
+        Arrays.sort(nums);
+        int[] ans=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            
+        }
+        return nums[3];
+    }
+        public static void main(String[] args){
+int[] ar={2,3,3,2,2,4,2,3,4};
+    System.out.println(minOperations(ar));        
+        }
+}
