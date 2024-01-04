00f36f9cab452ca713e1d69f8132c02125a5c75d
AnujLo1c <anujlowanshi3@gmail.com>
2024-01-04 13:49:35 +0530

    q2 done final

--- Lc2870.java
+++ Lc2870.java
@@ -7,25 +7,33 @@ class Lc2870 {
         int c=-1,count=0,t=0;
         for(int i=0;i<nums.length;i++){
             if(c!=nums[i]){
-            if(count>0){ans[t]=count;
-                t++;
-            count=0;
-        }
-            c=nums[i];
-            count++;
+                // System.out.println("countc"+count);
+                if(count>0){
+                    ans[t]=count;
+                    t++;
+                    count=0;
+                }
+                c=nums[i];
+                count++;
+                if(i==nums.length-1 && count==1){
+return -1;
+                }
+                
+                // System.out.println(" count= "+c+count);
             }
-            else if(c==nums[i]){
+            // else if(c==nums[i]){
+                else{
                 count++;
                 if(i==nums.length-1 && count>0){
                     ans[t]=count;
+                    t++;
                 }
             }
-            // System.out.print("c= "+c);
-            // System.out.println(" count= "+c);
-            // System.out.print(" count= "+c);
+        
         }
         c=0;count=0;
         for(int i=0;ans[i]!=0;i++){
+            System.out.println("index "+i+ans[i]);
             if(ans[i]==1) return -1;
             c=ans[i]%3;
             count+=(c==1||c==2)?((ans[i]/3)+1):ans[i]/3;
@@ -33,7 +41,7 @@ class Lc2870 {
         return count;
     }
         public static void main(String[] args){
-int[] ar={2,1,2,2,3,3};
+int[] ar={1,14,1,1,1};
     System.out.println(minOperations(ar));        
         }
 }
